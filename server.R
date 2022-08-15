b59b6aeb14540f8d71ebd710fe9ef82f22d4b725
PhilippBach <bach.philipp@outlook.com>
2022-08-15 13:57:52 +0200

    reorganize code

--- server.R
+++ server.R
@@ -3,68 +3,38 @@ shinyServer(function(input, output) {
   output$star_scatter_plot <- renderPlot({
 
     if (!input$moviestar) {
-      data_plot <- star_is_born
+      if (input$regression_line) {
+        plot <- plot_population_regressionline
+      } else {
+        plot  <- plot_population
+      }
     } else {
-      data_plot <- star_is_born %>%
-        filter(star == 1)
-    }
-
-    plot <- data_plot %>%
-      ggplot(aes(x = talent, y = beauty)) +
-      geom_point(size = 0.5, shape=23) +
-      xlim(-4, 4) + ylim(-4, 4) +
-      theme_minimal()
-
-    if (input$regression_line) {
-      plot <- plot +
-        stat_smooth(method = "lm", col = "red")
+      if (input$regression_line) {
+        plot <- plot_moviestar_regressionline
+      } else {
+        plot <- plot_moviestar
+      }
     }
     plot
   })
 
   output$moviestar_dag <- renderPlot({
     if (!input$moviestar) {
-      dag <- collider_triangle(x = "Talent", y = "Beauty", m = "Movie Star") %>%
-        ggdag_dseparated(
-          text = FALSE,
-          use_labels = "label",
-          node_size = 16,
-          text_size = 3.88,
-          label_size = text_size,
-          text_col = "#ffffff",
-          edge_type = "link_arc",
-          stylized = FALSE,
-          collider_lines = TRUE
-        )
+      dag <- collider_unconditional
     } else {
       ## Control for Movie star
-      dag <- collider_triangle(x = "Talent", y = "Beauty", m = "Movie Star") %>%
-        ggdag_dseparated(
-          text = FALSE,
-          use_labels = "label",
-          node_size = 16,
-          text_size = 3.88,
-          label_size = text_size,
-          text_col = "#ffffff",
-          edge_type = "link_arc",
-          stylized = FALSE,
-          collider_lines = TRUE,
-          controlling_for = "m"
-        )
+      dag <- collider_conditional
     }
     # ggdag(collider_dag) + theme_dag()
-    dag + theme_dag(panel.background = element_rect(fill = "#ffffff", colour = "#ffffff"))
+    dag
   })
 
   output$regression_tbl <- render_gt({
     if (!input$moviestar) {
-      data_plot <- star_is_born
+      reg_output <- reg_population
     } else {
-      data_plot <- star_is_born %>%
-        filter(star == 1)
+      reg_output <- reg_moviestar
     }
-    reg_moviestar <- lm(beauty ~ -1 + talent, data_plot)
-    as_gt(tbl_regression(reg_moviestar))
-
+    reg_output
   })
 })
