5f52bf26a5d5b8307ebebc770acd584b214e2297
PhilippBach <bach.philipp@outlook.com>
2022-08-25 12:40:29 +0200

    adjust alpha value in scatter plots

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -12,17 +12,18 @@ plot_population_regressionline <- star_is_born %>%
   xlim(-4, 4) + ylim(-4, 4) +
   theme_minimal()
 
-plot_moviestar <- star_is_born %>%
-  filter(star == 1) %>%
+plot_moviestar <- star_is_born_moviestar %>%
   ggplot(aes(x = talent, y = beauty)) +
-  geom_point(size = 0.5, shape=23) +
+  geom_point(size = 0.5, shape=23, alpha = 1) +
+   geom_point(size = 0.5, shape=23, alpha = 0.1, data = star_is_born[star_is_born$star == 0,]) +
   xlim(-4, 4) + ylim(-4, 4) +
   theme_minimal()
 
-plot_moviestar_regressionline <-  star_is_born %>%
-  filter(star == 1) %>%
+plot_moviestar_regressionline <- star_is_born_moviestar %>%
   ggplot(aes(x = talent, y = beauty)) +
-  geom_point(size = 0.5, shape=23) +
+  geom_point(size = 0.5, shape=23, alpha = 1) +
   stat_smooth(formula = y ~ x, method = "lm", col = "red") +
+  geom_point(size = 0.5, shape=23, alpha = 0.1, data = star_is_born[star_is_born$star == 0,]) +
   xlim(-4, 4) + ylim(-4, 4) +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "none")
