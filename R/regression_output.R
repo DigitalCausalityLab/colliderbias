fd5cd1da937d83a2ad2c2bdfbf62b4e6a190673b
PhilippBach <bach.philipp@outlook.com>
2022-08-25 12:37:37 +0200

    include intercept in lm() call

--- R/regression_output.R
+++ R/regression_output.R
@@ -1,11 +1,11 @@
 # Generate regression output
 reg_population <- star_is_born %>%
-  lm(formula = beauty ~ -1 + talent) %>%
+  lm(formula = beauty ~ talent) %>%
   tbl_regression %>%
   as_gt()
 
 reg_moviestar <- star_is_born %>%
   filter(star == 1) %>%
-  lm(formula = beauty ~ -1 + talent) %>%
+  lm(formula = beauty ~ talent) %>%
   tbl_regression %>%
   as_gt()
